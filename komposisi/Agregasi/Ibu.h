34488fb24ca03819ffc5cba9a05c7ebb23615ed7
Viaasyafrina16 <syafrinavia@gmail.com>
2024-05-21 16:08:33 +0700

    membuat int main untuk memanggil fungsi

--- komposisi/Agregasi/Ibu.h
+++ komposisi/Agregasi/Ibu.h
@@ -2,6 +2,30 @@
 #define IBU_H
 
 class ibu {
-public :
+    public:
+    string nama;
+    vector<anak*>daftar_anak;
 
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \" " << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \" " << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+
+};
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
 }
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    } 
+    cout << endl;
+}
+#endif
